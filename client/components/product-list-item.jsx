5d1970222e7678deda0cd350fac3679c04f5f984
Strebig <strebig.nick@gmail.com>
2019-05-23 19:24:55 +0000

    Fixed image height on main page

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -16,7 +16,7 @@ export default class ProductListItem extends React.Component {
     return (
       <div className="card-deck">
         <div className="card" >
-          <img className="card-img-top" src={this.props.products.image} alt="Product Image" onClick={this.props.setView}></img>
+          <img className="card-img-top main-height" src={this.props.products.image} alt="Product Image" onClick={this.props.setView}></img>
           <div className="card-body">
             <h5 className="card-title">{this.props.products.name}</h5>
             <h5 className="card-title">${this.props.products.price / 100}</h5>
