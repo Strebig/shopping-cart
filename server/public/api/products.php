eba0f353a020c5b396e54cb59c35973e4e8e88e0
Strebig <strebig.nick@gmail.com>
2019-06-03 22:49:38 +0000

    .

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -1,8 +1,13 @@
 <?php
+require_once('mysql_credentials.php');
+
 
 header('Content-Type: application/json');
 
 if (empty($_GET['id'])) {
+  // $query = 'SELECT * FROM product_details';
+  // $results = $conn->query($query);
+  // return $results;
   readfile('dummy-products-list.json');
 } else {
   readfile('dummy-product-details.json');
