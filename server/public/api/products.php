b3cab959a9bcb34203a5231f7ccd289b7184af98
Strebig <strebig.nick@gmail.com>
2019-06-04 01:26:49 +0000

    Updated PHP and Connected Database data instead of dummy JSON data

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -1,16 +1,29 @@
 <?php
 require_once('mysql_credentials.php');
 
-
 header('Content-Type: application/json');
 
+$query = 'SELECT * FROM product_details';
+$results = mysqli_query($conn, $query);
+
+if (!$results) {
+  throw new Exception (mysqli_error($conn));
+}
+$output = [];
+
+while ($row = mysqli_fetch_assoc($results)) {
+  $output[] = $row;
+}
+
+$json_output = json_encode($output);
+
 if (empty($_GET['id'])) {
-  // $query = 'SELECT * FROM product_details';
-  // $results = $conn->query($query);
-  // return $results;
-  readfile('dummy-products-list.json');
+  print($json_output);
+  // readfile('dummy-products-list.json');
 } else {
   readfile('dummy-product-details.json');
 }
+
+
 
 ?>
